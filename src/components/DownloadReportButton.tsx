1ae584fd1620c02dccb1031545dc7e8d9424454a
KavishkaSash <kavishka@teatang.com>
2024-12-06 17:02:16 +0530

    Add StraightLine and BlendBalance components;

--- src/components/DownloadReportButton.tsx
+++ src/components/DownloadReportButton.tsx
@@ -541,7 +541,7 @@ const DownloadReportButton: React.FC<DownloadReportButtonProps> = ({
       ),
       theme: 'plain', // Clean layout
 
-      startY: 200,
+      startY: 150,
       tableWidth: pageWidth - 3 * margin,
       margin: { left: margin, right: margin },
 
