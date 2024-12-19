425336854297f9618f314c75cd5f836e9359f980
KavishkaSash <kavishka@teatang.com>
2024-12-19 11:11:11 +0530

    Add framer-motion for sidebar animation; refactor Sidebar component to use motion.div for smooth width transitions

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -13,6 +13,8 @@ import {
   ChevronRight,
   PanelRightClose,
   PanelRightOpen,
+  IndentDecrease,
+  Menu,
 } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
@@ -20,6 +22,7 @@ import { cn } from '@/lib/utils'
 import { usePathname, useRouter } from 'next/navigation'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { createBrowserClient } from '@/utils/supabase'
+import { motion } from 'framer-motion'
 
 const Sidebar: React.FC<{
   onSnowfallToggle: (enabled: boolean) => void
@@ -72,132 +75,142 @@ const Sidebar: React.FC<{
   ]
 
   return (
-    <div
+    <motion.div
+      initial={{ width: isExpanded ? 64 : 16 }} // Initial sidebar width
+      animate={{ width: isExpanded ? 256 : 64 }} // Animates width
+      transition={{ type: 'spring', stiffness: 150, damping: 15 }} // Smooth animation
       className={cn(
-        'duration-20 flex h-full flex-col bg-gray-100 text-gray-800 transition-all ease-in-out',
-        isExpanded ? 'w-64' : 'w-16',
+        'duration-20 flex h-full flex-col bg-gray-100 text-gray-800',
       )}
-      // onMouseEnter={() => handleExpand(true)}
-      // onMouseLeave={() => handleExpand(false)}
     >
-      <div className="flex justify-center p-2">
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => handleExpand(!isExpanded)}
-          className="h-6 w-6 rounded-full hover:bg-gray-200"
-        >
-          {isExpanded ? (
-            <ChevronLeft className="h-8 w-8" />
-          ) : (
-            <ChevronRight className="h-8 w-8" />
-          )}
-        </Button>
-      </div>
-      <div className="mb-8 flex flex-col items-center p-4">
-        <img
-          src="/Tea_Tang_-_HighRes_Logo2.png"
-          alt="Logo"
-          className={cn('mb-2', isExpanded ? 'h-12 w-auto' : 'h-8 w-auto')}
-        />
-        <span
-          className={cn(
-            'font-semibold text-gray-800 transition-all',
-            isExpanded ? 'text-lg' : 'text-s',
-          )}
-        >
-          {isExpanded ? 'Tea Management' : 'TM'}
-        </span>
-      </div>
-      <nav className="flex-1 px-2">
-        <ul className="space-y-2">
-          {menuItems.map((item) => (
-            <li key={item.name}>
-              <Link href={item.route} className="block">
-                <Button
-                  variant="ghost"
-                  className={cn(
-                    'w-full justify-start rounded-lg text-left !text-gray-800 transition-colors hover:bg-gray-200',
-                    isExpanded ? 'my-5 px-2 py-7' : 'my-5 px-2 py-7',
-                    'flex items-center',
-                    pathname === item.route && 'bg-gray-200',
-                  )}
-                >
-                  <span
+      <div
+        className={cn(
+          'duration-20 flex h-full flex-col bg-gray-100 text-gray-800 transition-all ease-in-out',
+          isExpanded ? 'w-64' : 'w-16',
+        )}
+        // onMouseEnter={() => handleExpand(true)}
+        // onMouseLeave={() => handleExpand(false)}
+      >
+        <div className="flex justify-center p-2">
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => handleExpand(!isExpanded)}
+            className="h-10 w-10  hover:bg-gray-200"
+          >
+            {isExpanded ? (
+              <IndentDecrease className="h-8 w-8" />
+            ) : (
+              <Menu className="h-8 w-8" />
+            )}
+          </Button>
+        </div>
+        <div className="mb-8 flex flex-col items-center p-4">
+          <img
+            src="/Tea_Tang_-_HighRes_Logo2.png"
+            alt="Logo"
+            className={cn('mb-2', isExpanded ? 'h-12 w-auto' : 'h-8 w-auto')}
+          />
+          <span
+            className={cn(
+              'font-semibold text-gray-800 transition-all',
+              isExpanded ? 'text-lg' : 'text-s',
+            )}
+          >
+            {isExpanded ? 'Tea Management' : 'TM'}
+          </span>
+        </div>
+        <nav className="flex-1 px-2">
+          <ul className="space-y-2">
+            {menuItems.map((item) => (
+              <li key={item.name}>
+                <Link href={item.route} className="block">
+                  <Button
+                    variant="ghost"
                     className={cn(
+                      'w-full justify-start rounded-lg text-left !text-gray-800 transition-colors hover:bg-gray-200',
+                      isExpanded ? 'my-5 px-2 py-7' : 'my-5 px-2 py-7',
                       'flex items-center',
-                      isExpanded ? 'mr-3' : 'mr-0',
+                      pathname === item.route && 'bg-gray-200',
                     )}
                   >
-                    {item.icon}
-                  </span>
-                  {isExpanded && <span>{item.name}</span>}
-                </Button>
-              </Link>
-            </li>
-          ))}
+                    <span
+                      className={cn(
+                        'flex items-center',
+                        isExpanded ? 'mr-3' : 'mr-0',
+                      )}
+                    >
+                      {item.icon}
+                    </span>
+                    {isExpanded && <span>{item.name}</span>}
+                  </Button>
+                </Link>
+              </li>
+            ))}
 
-          {/* Snowfall Toggle */}
-          <li>
-            <Button
-              variant="ghost"
-              onClick={handleSnowfallToggle}
-              className={cn(
-                'w-full justify-start rounded-lg text-left !text-gray-800 transition-colors hover:bg-gray-200',
-                isExpanded ? 'my-5 px-2 py-7' : 'my-5 px-2 py-7',
-                'flex items-center',
-              )}
-            >
-              <span
+            {/* Snowfall Toggle */}
+            <li>
+              <Button
+                variant="ghost"
+                onClick={handleSnowfallToggle}
                 className={cn(
+                  'w-full justify-start rounded-lg text-left !text-gray-800 transition-colors hover:bg-gray-200',
+                  isExpanded ? 'my-5 px-2 py-7' : 'my-5 px-2 py-7',
                   'flex items-center',
-                  isExpanded ? 'mr-3' : 'mr-0',
                 )}
               >
-                <Snowflake
-                  className={
-                    isSnowfallEnabled ? 'text-blue-400' : 'text-gray-400'
-                  }
-                />
-              </span>
-              {isExpanded && (
-                <span>
-                  {isSnowfallEnabled ? 'Disable Snow' : 'Enable Snow'}
+                <span
+                  className={cn(
+                    'flex items-center',
+                    isExpanded ? 'mr-3' : 'mr-0',
+                  )}
+                >
+                  <Snowflake
+                    className={
+                      isSnowfallEnabled ? 'text-blue-400' : 'text-gray-400'
+                    }
+                  />
                 </span>
-              )}
-            </Button>
-          </li>
-        </ul>
-      </nav>
+                {isExpanded && (
+                  <span>
+                    {isSnowfallEnabled ? 'Disable Snow' : 'Enable Snow'}
+                  </span>
+                )}
+              </Button>
+            </li>
+          </ul>
+        </nav>
 
-      <div className="flex flex-col items-center justify-center p-4">
-        <Avatar className="mb-2 h-10 w-10">
-          <AvatarImage
-            src={
-              user?.user_metadata?.avatar_url || 'https://picsum.photos/200/300'
-            }
-            alt="User Avatar"
-          />
-          <AvatarFallback>
-            {user?.email?.charAt(0).toUpperCase() || 'U'}
-          </AvatarFallback>
-        </Avatar>
-        {isExpanded && (
-          <>
-            <span className="mb-2 text-sm text-gray-600">
-              {user?.email || 'Loading...'}
-            </span>
-            <Button
-              className="mt-2 flex w-full items-center justify-center rounded-md bg-red-500 px-4 py-2 text-white hover:bg-red-600"
-              onClick={handleSignOut}
-            >
-              <LogOut className="mr-2" size={16} />
-              Logout
-            </Button>
-          </>
-        )}
+        <div className="flex flex-col items-center justify-center p-4">
+          <Avatar className="mb-2 h-10 w-10">
+            <AvatarImage
+              src={
+                user?.user_metadata?.avatar_url ||
+                'https://picsum.photos/200/300'
+              }
+              alt="User Avatar"
+            />
+            <AvatarFallback>
+              {user?.email?.charAt(0).toUpperCase() || 'U'}
+            </AvatarFallback>
+          </Avatar>
+          {isExpanded && (
+            <>
+              <span className="mb-2 text-sm text-gray-600">
+                {user?.email || 'Loading...'}
+              </span>
+              <Button
+                className="mt-2 flex w-full items-center justify-center rounded-md bg-red-500 px-4 py-2 text-white hover:bg-red-600"
+                onClick={handleSignOut}
+              >
+                <LogOut className="mr-2" size={16} />
+                Logout
+              </Button>
+            </>
+          )}
+        </div>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
