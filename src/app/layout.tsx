4ed9d77449151b4795947ed22a6d2faa8e9986c1
KavishkaSash <kavishka@teatang.com>
2025-01-03 16:00:37 +0530

    Removed SnowFall

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,14 +47,14 @@ export default function RootLayout({
             {process.env.NEXT_PUBLIC_API_BASE_URL}
           </div>
         )}
-        {isSnowfallEnabled && snowflake2 && (
+        {/* {isSnowfallEnabled && snowflake2 && (
           <Snowfall
             snowflakeCount={20}
             color="#d4d4d4"
             images={images}
             radius={[15.0, 18.0]}
           />
-        )}
+        )} */}
 
         <NextTopLoader showSpinner={false} height={2} color="#2acf80" />
         <ThemeProvider
